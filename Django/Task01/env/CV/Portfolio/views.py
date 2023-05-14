9978f9d02de8c7328acbf975d1cb18343bca4be8
Abdallah <abdallah.younis0001@gmail.com>
2023-05-14 16:29:22 +0300

    Task02

--- Django/Task01/env/CV/Portfolio/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.template import loader
-from django.http import HttpResponse
-from django.http import request
-
-def Welcome(request):
-	return render(request, 'show.html')
-
-
